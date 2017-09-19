0fe087b9cba0a1a49f89d40e69483932bcc740f0
User-ПК\User <crab_@mail.ru>
2017-09-19 17:39:01 +0300

    no message

--- src/man.java
+++ src/man.java
@@ -4,6 +4,16 @@ public class man {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int s = 864000;
+		int sec = s % 60;
+		int m = (s - sec) / 60;
+		int min = m % 60;
+		int h = (m - min) / 60;
+		int hr = h % 24;
+		int d = (h - hr) / 24;
+		int day = d % 7;
+		int w = (d - day) / 7;
+		System.out.println(w + " недель " + day + " дней " + hr + " часов " + min + " минут " + sec + " секунд ");
 	}
 
 }
