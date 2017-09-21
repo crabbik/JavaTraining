8867caa8db6dd4e0c2c5d5b6c5ee683e14ba325c
olegsergeev <olehrubchinskiy@gmail.com>
2017-09-21 20:38:01 +0300

    no message

---
+++ src/taskIN/task3.java
@@ -0,0 +1,36 @@
+package taskIN;
+
+import java.util.Arrays;
+
+public class task3 {
+
+	public static void main(String[] args) {
+
+		int marks[] = { 1, 7, 2, 78, 3, 3, 7, 9, 43, 5, 6, 4, 3, 7, 4, 8 };
+
+		int maxMark = marks[0];
+		int x = 0;
+		for (int i = 0; i < marks.length; i++) {
+			if (maxMark < marks[i]) {
+				maxMark = marks[i];
+				x = i;
+			}
+
+		}
+		int minMark = marks[0];
+		int c = 0;
+		for (int a = 0; a < marks.length; a++) {
+			if (minMark > marks[a]) {
+				minMark = marks[a];
+				c = a;
+			}
+
+		}
+		int q = marks[x];
+		marks[x] = marks[c];
+		marks[c] = q;
+		System.out.println(Arrays.toString(marks));
+
+	}
+
+}
---
+++ src/taskIN/task4.java
@@ -0,0 +1,17 @@
+package taskIN;
+
+public class task4 {
+
+	public static void main(String[] args) {
+
+		int v = 3;
+		int sum = 0;
+		int i = 1;
+		while (i <= v) {
+			sum += i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+
+}
