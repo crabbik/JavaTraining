356ae93abdffb86ee3745210c8a15086478fbf20
olegsergeev <olehrubchinskiy@gmail.com>
2017-10-24 20:48:12 +0300

    no message

---
+++ src/threadTest3/Test.java
@@ -0,0 +1,25 @@
+package threadTest3;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		final ScheduledExecutorService scheduler = Executors
+				.newScheduledThreadPool(2);
+		Runnable runnable = new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("Прошло 10 секунд");
+
+			}
+		};
+		scheduler.scheduleAtFixedRate(runnable, 1, 10, TimeUnit.SECONDS);
+
+	}
+
+}
